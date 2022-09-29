a0c82ce27ba55f805cabba7551d8bcad6da4b54d
Rubayet-billah <rubayetbillah909@gmail.com>
2022-09-29 12:21:36 +0600

    component added

--- src/components/Clculation/Calculation.js
+++ src/components/Clculation/Calculation.js
@@ -1,27 +1,12 @@
 import React from 'react';
+import Personal from '../Personal/Personal';
 
 const Calculation = () => {
     return (
         <div className='bg-light'>
+            <Personal></Personal>
             <div>
-                <h1>dhvfh</h1>
-                <img src="images/profile1.jpg" alt="" />
-                <h3>Rubayet Billah</h3>
-                <p>Khulna,Bangladesh</p>
-            </div>
-            <div>
-                <div>
-                    <span className='h3'>68</span><span>kg</span>
-                    <p>Weight</p>
-                </div>
-                <div>
-                    <span className='h3'>5.8</span><span>ft</span>
-                    <p>Height</p>
-                </div>
-                <div>
-                    <span className='h3'>23</span><span>yrs</span>
-                    <p>Age</p>
-                </div>
+                <h3>Add A Break</h3>
             </div>
         </div>
     );
---
+++ src/components/Personal/Personal.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Personal = () => {
+    return (
+        <div>
+            <div>
+                <h1>dhvfh</h1>
+                <img src="images/profile1.jpg" alt="" />
+                <h3>Rubayet Billah</h3>
+                <p>Khulna,Bangladesh</p>
+            </div>
+            <div className='d-flex'>
+                <div>
+                    <span className='h3'>68</span><span>kg</span>
+                    <p>Weight</p>
+                </div>
+                <div>
+                    <span className='h3'>5.8</span><span>ft</span>
+                    <p>Height</p>
+                </div>
+                <div>
+                    <span className='h3'>23</span><span>yrs</span>
+                    <p>Age</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Personal;
