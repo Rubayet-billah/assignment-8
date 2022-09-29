3ae9630ee9424ec5b96c8d6dcc85b520b75e2912
Rubayet-billah <rubayetbillah909@gmail.com>
2022-09-29 11:44:05 +0600

    card styled

--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -4,12 +4,14 @@ const Subject = (props) => {
     const { id, name, img, category, credit, time } = props.subject;
     return (
         <div className='col-md-6 col-lg-4'>
-            <div>
-                <img className='img-fluid' src={img} alt="" />
-                <h3>Subject: {name}</h3>
-                <p>Category: {category}</p>
-                <p>Credit: {credit}</p>
-                <p>Time: {time} h</p>
+            <div className='border border-2 rounded-2'>
+                <img className='img-fluid rounded-2' src={img} alt="" />
+                <div className='px-3'>
+                    <h3>{name}</h3>
+                    <p>Category: {category}</p>
+                    <p>Credit: {credit}</p>
+                    <p>Time: {time} h</p>
+                </div>
             </div>
         </div>
     );
